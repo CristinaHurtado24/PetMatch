3da1151481a85233f3aaa63f1b6fc5a804a637bd
Cristina Hurtado <cristyhurtado64@gmail.com>
2022-10-09 11:56:55 -0400

    testing

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import Main from './src/components/Main.jsx'
-import { NativeRouter } from 'react-router-native';
+import React from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import Main from "./src/components/Main.jsx";
+import Onboarding from "./src/components/Onboarding.jsx";
+import { NativeRouter } from "react-router-native";
 
 export default function App() {
   return (
-    <NativeRouter>
-      <Main/>
-    </NativeRouter>
+    <View styles={styles.container}>
+      <></>
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flew: 1,
+    backgroundColor:"#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ slides.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    id: "1",
+    title: "Facilidad",
+    description: "Encuentra mascotas con las características que buscas.",
+    image:
+      "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg",
+  },
+  {
+    id: "2",
+    title: "Matches",
+    description: "Podrás ver los matches que hagas con otras mascotas.",
+    image:
+      "https://st.depositphotos.com/1007630/2727/i/450/depositphotos_27276047-stock-photo-portrait-of-two-young-beauty.jpg",
+  },
+  {
+    id: "3",
+    title: "Haz amigos",
+    description:
+      "Aprovecha al máximo y encuentra amigos con mascotas y hobbies en común.",
+    image:
+      "https://www.valenciabonita.es/wp-content/uploads/2019/06/playas-para-perros-en-la-Comunidad-Valenciana-en-2019.jpg",
+  },
+];
