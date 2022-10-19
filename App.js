2fe734ba1a36027b21602faaceedb7d5694e07e3
Cristina Hurtado <cristyhurtado64@gmail.com>
2022-10-18 21:14:12 -0400

    landing para el registro listo

--- App.js
+++ App.js
@@ -1,41 +1,14 @@
 import React, {useState, useEffect} from "react";
 import { View, Text, StyleSheet, StatusBar } from "react-native";
 import Onboarding from "./src/components/Onboarding.js";
-import HomeScreen from "./src/pages/HomeScreen.js";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { ActivityIndicator } from "react-native-web";
+import LandingRegister from "./src/components/LandingRegister.js";
 
-const Loading=()=>{
-  <View>
-    <ActivityIndicator size='large'/>
-  </View>
-}
 
 export default function App() {
-  // const [loading, setLoading] = useState(true);
-  // const [viewedOnboarding, setViewedOnboarding] = useState(false);
-
-  // const checkOnboarding = async()=> {
-  //   try {
-  //     const value = await AsyncStorage.getItem('@viewedOnboarding');
-
-  //     if (value !== null) {
-  //       setViewedOnboarding(true)
-  //     }
-  //   } catch (error) {
-  //     console.log('Error @checkOnboarding: ', err)
-  //   } finally {
-  //     setLoading(false)
-  //   }
-  // }
-
-  // useEffect(()=>{
-  //   checkOnboarding();
-  // },[])
+  
   return (
     <View style={styles.container}>
-      <Onboarding/>
-      {/* {loading ? <Loading /> : viewedOnboarding ? <HomeScreen/> : <Onboarding/>} */}
+      <LandingRegister/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/LandingRegister.js
@@ -0,0 +1,92 @@
+import { View, Text, Image, StyleSheet, useWindowDimensions, Button, Alert } from 'react-native'
+import React from 'react'
+import { AntDesign } from "@expo/vector-icons";
+
+export default function RegisterOwner() {
+  const { width } = useWindowDimensions();
+  return (
+    <View>
+      <View>
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://media.istockphoto.com/vectors/paw_print-vector-id931785704?k=20&m=931785704&s=612x612&w=0&h=wpnhxlh6HW0tRBxVIWynZMuJ-Lpp5rDRWjlVL2y_nt8=",
+          }}
+        />
+      </View>
+      <Text style={styles.title}>Registrate para continuar</Text>
+      <View style={styles.button}>
+        <Button
+          onPress={() => {
+            Alert.alert("Nice");
+          }}
+          title="Continua con email"
+          color="#ffff"
+        />
+      </View>
+      <Text style={styles.text}>O registrate con</Text>
+      <View style={styles.options}>
+        <AntDesign
+          name="twitter"
+          size={40}
+          color="#493d8a"
+          onPress={() => {
+            Alert.alert("Inicio con Twitter");
+          }}
+        />
+        <AntDesign 
+          name="google" 
+          size={40} 
+          color="#493d8a" 
+          onPress={()=>{
+            Alert.alert("Inicio con google")
+        }}/>
+        <AntDesign 
+          name="facebook-square" 
+          size={40} 
+          color="#493d8a" 
+          onPress={()=>{
+            Alert.alert("Inicio con Facebook")
+        }}/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    flex: 0.65,
+    marginTop: 40
+  },
+  title: {
+    fontWeight: "800",
+    fontSize: 22,
+    marginBottom: 10,
+    marginTop: -40,
+    color: "black",
+    textAlign: "center",
+  },
+  text: {
+    fontWeight: "300",
+    color: "#62656b",
+    textAlign: "center",
+    paddingHorizontal: 64,
+    fontSize: 18,
+    marginTop:20
+  },
+  button: {
+    backgroundColor:"#493d8a",
+    borderRadius: 10,
+    marginTop:15,
+  },
+  options:{
+    flexDirection:'row',
+    justifyContent: 'space-around',
+    marginTop: 40
+  }
+});
