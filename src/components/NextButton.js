3bf9262ce1087501552c190a00e63f742a50aaac
Cristina Hurtado <cristyhurtado64@gmail.com>
2022-10-24 12:56:28 -0400

    cambios

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+
 
 export default NextButton = ({navigation}) => {
   return (
