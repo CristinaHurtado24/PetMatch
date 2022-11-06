f742395135a604ae060aca9782301bbc45083f3b
Cristina Hurtado <cristyhurtado64@gmail.com>
2022-11-06 01:40:56 -0400

    bug searchBar

--- src/components/MatchDiscover.js
+++ src/components/MatchDiscover.js
@@ -1,18 +1,55 @@
 import * as React from "react";
-import * as RN from "react-native";
+import {Alert}from "react-native";
 import { database } from "../config/fb";
 import { useNavigation } from "@react-navigation/native";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { Searchbar } from "react-native-paper";
+import SearchableDropdown from "react-native-searchable-dropdown";
 
 import PetCard from "./PetCard";
 import { StyleSheet, View, Dimensions, ScrollView } from "react-native";
 
 const { width, height } = Dimensions.get("window");
 
+//Item array for the dropdown
+const items = [
+  //name key is must.It is to show the text in front
+  { id: 1, name: "Raza" },
+  { id: 2, name: "Ciudad" },
+];
 export default Match = () => {
   const navigation = useNavigation();
 
   const [products, setProducts] = React.useState([]);
+  const [productsA, setProductsA] = React.useState([]);
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const [searchFilter, setSearchFilter] = React.useState("");
+
+  const onChangeFilter = (selected) => {
+    setSearchFilter(selected.id);
+    console.log("Filter id");
+    console.log(searchFilter);
+  };
+  const onChangeSearch = (query) => {
+    setSearchQuery(query);
+    console.log(searchQuery)
+    const helper = [];
+    if (searchQuery != "" && searchFilter != "") {
+      if (searchFilter == 1) {
+        //Busca por raza
+        for (let index = 0; index < products.length; index++) {
+          const element = products[index];
+          if (element.raza === searchQuery) {
+            helper.push(element);
+          }
+        }
+      } else {
+        //Busca por ciudad
+      }
+      setProductsA(helper)
+    } else if (searchFilter == "") {
+      Alert.alert("Seleccione un filtro");}
+  };
 
   React.useEffect(() => {
     const collectionRef = collection(database, "products");
@@ -39,11 +76,67 @@ export default Match = () => {
   }, []);
 
   return (
-    <ScrollView>
-      {products.map((product) => (
-        <PetCard key={product.id} {...product} />
-      ))}
-    </ScrollView>
+    <>
+      <View style={styles.search}>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+          icon="magnify"
+          style={styles.searchbar}
+        />
+        <SearchableDropdown
+          onTextChange={(text) => console.log(text)}
+          //On text change listner on the searchable input
+          onItemSelect={onChangeFilter}
+          //onItemSelect called after the selection from the dropdown
+          containerStyle={{ padding: 5 }}
+          //suggestion container style
+          textInputStyle={{
+            //inserted text style
+            padding: 12,
+            borderWidth: 1,
+            borderColor: "#ccc",
+            backgroundColor: "#FAF7F6",
+          }}
+          itemStyle={{
+            //single dropdown item style
+            padding: 10,
+            marginTop: 2,
+            backgroundColor: "#FAF9F8",
+            borderColor: "#bbb",
+            borderWidth: 1,
+          }}
+          itemTextStyle={{
+            //text style of a single dropdown item
+            color: "#222",
+          }}
+          itemsContainerStyle={{
+            //items container style you can pass maxHeight
+            //to restrict the items dropdown hieght
+            maxHeight: "60%",
+          }}
+          items={items}
+          placeholder="Filtro"
+          //place holder for the search input
+          resetValue={false}
+          //reset textInput Value with true and false state
+        />
+      </View>
+      {productsA.length!=0?
+         <ScrollView>
+          {productsA.map((product) => (
+            <PetCard key={product.id} {...product} />
+          ))}
+        </ScrollView>
+      :
+        <ScrollView>
+          {products.map((product) => (
+            <PetCard key={product.id} {...product} />
+          ))}
+        </ScrollView>
+      }
+    </>
   );
 };
 
@@ -84,5 +177,11 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     marginTop: 40,
+  },
+  search: {
+    flexDirection: "row",
+  },
+  searchbar: {
+    width: 350,
   },
 });
