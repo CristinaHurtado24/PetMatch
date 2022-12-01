3e9637ce2d5374322502190c3a96d6d006cdaa7b
Massimo Tassinari <massimotassinariaulisi@gmail.com>
2022-12-01 00:21:10 -0400

    finallllllll

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -44,7 +44,6 @@ export default PetCard = ({
   var userprof = "";
   const [productsA, setProductsA] = React.useState([]);
 
-
   React.useEffect(() => {
     const collectionRef = collection(database, "products");
     const q = query(collectionRef, orderBy("name", "desc"));
@@ -52,7 +51,7 @@ export default PetCard = ({
     const unsuscribe = onSnapshot(q, (querySnapshot) => {
       setProducts(
         querySnapshot.docs.map((doc) => ({
-          id: doc.id,
+          uid: doc.data().uid,
           email: doc.data().email,
           password: doc.data().password,
           name: doc.data().name,
@@ -115,7 +114,7 @@ export default PetCard = ({
       const elements = products[index];
       //console.log("entra");
       //console.log(elements);
-      if ( route.params.userEmail === elements.email) {
+      if (route.params.userEmail === elements.email) {
         var userprofmatch = elements;
         console.log(userprofmatch);
         //setProductsA(element);
@@ -127,10 +126,10 @@ export default PetCard = ({
         //const uniqueId = userprofmatch.id;
         //await database.collection("products").doc(uniqueId).update()
       }
-      } return userprofmatch;
     }
+    return userprofmatch;
+  }
 
-    
   const navigation = useNavigation();
   const collectionRef = collection(database, "products");
   const onSend = async () => {
@@ -144,58 +143,70 @@ export default PetCard = ({
   async function actualizar2(b) {
     //const f = "Andrea@gmail.com"
     const uniqueId = b.uid;
-    const usuariosRef = doc(collection(database, "products"), uniqueId)
-    await updateDoc(usuariosRef, { sent: b.sent  });}
+    const usuariosRef = doc(collection(database, "products"), uniqueId);
+    await updateDoc(usuariosRef, { sent: b.sent });
+  }
 
   return (
     <View style={styles.all}>
       <View style={styles.cardContainer}>
         <Image style={styles.imageStyle} source={{ uri: url }}></Image>
-      <Text style={styles.name}>{dogName}</Text>
-      <Text style={styles.raza}>{raza}</Text>
-      <View style={styles.icons}>
-        <AntDesign
-          name="heart"
-          size={50}
-          color="#941DE8"
-          onPress={() => {
-            Alert.alert("Enviada la solicitud");
-            console.log(email);
-            //console.log(b);
-            const a = buscar(email);
-            //const b = buscar2(email)
-            //actualizar2(b);
-            //match.push(userprof);
-            //console.log(match)
-            //{onSend}
-            actualizar(a);
-            //actualizar_2(b);
-            async function actualizar(a) {
+        <Text style={styles.name}>{dogName}</Text>
+        <Text style={styles.raza}>{raza}</Text>
+        <View style={styles.icons}>
+          <AntDesign
+            name="heart"
+            size={50}
+            color="#941DE8"
+            onPress={() => {
+              Alert.alert("Enviada la solicitud");
+              console.log(email);
+              //console.log(b);
+              const a = buscar(email);
+              //const b = buscar2(email)
+              //actualizar2(b);
+              //match.push(userprof);
+              //console.log(match)
+              //{onSend}
+              //const uniqueId = a.id;
+              console.log("*******");
+              //console.log(uniqueId);
+              console.log("*******");
+              actualizar(a);
+              //actualizar_2(b);
+
+              async function actualizar(a) {
                 const uniqueId = a.uid;
-                const usuariosRef = doc(collection(database, "products"), uniqueId)
-                await updateDoc(usuariosRef, { requests: a.requests });}
-            //async function actualizar_2(b) {
-                //const uniqueId = b.uid;
-                //const usuariosRef = doc(collection(database, "products"), uniqueId)
-                //await updateDoc(usuariosRef, { sent: b.sent });}
-                //console.log(a.id)
-                //DocumentReference docRef = db.collection("cities").document("DC");
-                // await database.collection("products").doc(uniqueId).update(
-                //   {
-                //     id : a.id,
-                //     name : a.name,
-                //     lastName : a.lastName,
-                //     email: a.email,
-                //     password: a.password,
-                //     phone: a.password,
-                //     dogName: a.dogName,
-                //     raza: a.raza,
-                //     dogAge: a.dogAge,
-                //     dogSex: a.dogSex,
-                //     url: a.url,
-                //     requests: a.requests,
-                //   }
-                //)
+                console.log(`#####&&& props.id: ${uniqueId}`);
+                const usuariosRef = doc(
+                  collection(database, "products"),
+                  uniqueId
+                );
+                await updateDoc(usuariosRef, { requests: a.requests });
+              }
+
+              //async function actualizar_2(b) {
+              //const uniqueId = b.uid;
+              //const usuariosRef = doc(collection(database, "products"), uniqueId)
+              //await updateDoc(usuariosRef, { sent: b.sent });}
+              //console.log(a.id)
+              //DocumentReference docRef = db.collection("cities").document("DC");
+              // await database.collection("products").doc(uniqueId).update(
+              //   {
+              //     id : a.id,
+              //     name : a.name,
+              //     lastName : a.lastName,
+              //     email: a.email,
+              //     password: a.password,
+              //     phone: a.password,
+              //     dogName: a.dogName,
+              //     raza: a.raza,
+              //     dogAge: a.dogAge,
+              //     dogSex: a.dogSex,
+              //     url: a.url,
+              //     requests: a.requests,
+              //   }
+              //)
             }}
           />
           <AntDesign
